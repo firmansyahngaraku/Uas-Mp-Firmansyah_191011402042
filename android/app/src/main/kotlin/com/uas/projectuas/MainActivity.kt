023d0791ac49c7d1c4965bcfe49802a9f525a543
firmansyahngaraku <paskersila1980>
2022-04-09 23:23:29 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/uas/projectuas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.uas.projectuas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/notes_operation.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:projectuas/note.dart';
+import 'package:projectuas/Json_convert.dart';
+
+class NotesOperation with ChangeNotifier {
+  //List of note
+  List<Note> _notes = [];
+
+  List<Note> get getNotes {
+    return _notes;
+  }
+
+  NotesOperation() {
+    addNewNote('First Note', 'First Note Description');
+  }
+
+  void addNewNote(String title, String description) {
+    //Note object
+    Note note = Note(title, description);
+    _notes.add(note);
+    notifyListeners();
+  }
+}
